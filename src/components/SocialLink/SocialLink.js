0986a47cc78f8b3e19ab8c36e87391a146c88912
Piotr Łosiak <piotr.losiak96@gmail.com>
2020-05-19 18:46:11 +0200

    ref #1 Team Members

---
+++ src/components/SocialLink/SocialLink.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './css/fontello.css';
+
+export const socialClasses = {
+    github: 'icon-github-circled',
+    linkedin: 'icon-linkedin-squared'
+}
+
+function SocialLink({ url,type }) {
+    return (
+        <div className="social-link">
+            <a href={url}>
+                <span className={socialClasses[type]}></span>
+            </a>
+        </div>
+    );
+}
+
+export default SocialLink;
